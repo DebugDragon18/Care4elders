fb9c932ab9e4bc084c7aad2b0f5d6e3eff97e5ca
Rugved Narkar 78 <narkarrugved100@gmail.com>
2025-08-24 17:21:33 +0000

    I think there is problem in gemini api can we use chatgpt api instead of

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+      model: 'gemini-1.5-flash-latest',
+    }),
+  ],
 });

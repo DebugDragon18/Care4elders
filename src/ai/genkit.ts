ce4f49eb97c16aa8484dbf6678985ec82f7e5991
Rugved Narkar 78 <narkarrugved100@gmail.com>
2025-08-27 16:15:56 +0000

    Error Still not resolved please suggest permanant solution

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,13 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import { config } from 'dotenv';
+
+config();
 
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: process.env.GEMINI_API_KEE,
       model: 'gemini-1.5-flash-latest',
     }),
   ],
